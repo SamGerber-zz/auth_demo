898d97d3a2cd75d2bfcd89fd40610118f89088b2
SamGerber <gerbyxvi@gmail.com>
2016-01-27 07:51:41 -0800

    Switch to password_digest from password
     - Update validations
     - Implement ::find_by_credentials to fetch User based on username and password
       digest.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-  validates :username, :password, presence: true
-  validates :password, length: { minimum: 6 }
+  validates :username, :password_digest, presence: true
+  #validates :password, presence: true, length: { minimum: 6 }
+
+  def self.find_by_credentials(username:, password:)
+    #TODO
+
+    user = find_by(username: username)
+    return nil if user.blank?
+    password_digest = Digest::SHA2.hexdigest(password)
+    user.password_digest == password_digest ? user : nil
+  end
 end
