80e1790305308c2a04ada8727a4b62adc07eab90
SamGerber <gerbyxvi@gmail.com>
2016-01-27 06:46:07 -0800

    Add session helper methods
    Implement ApplicationController#current_user
    Implement ApplicationController#log_in!(user)
    Implement ApplicationController#log_out!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :exception
+
+
+  # Fetches the user we are logged in as
+  def current_user
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id])
+  end
+
+  # Log a user out
+  def log_in!(user)
+    session[:user_id] = user.id
+  end
+
+  # Log a user out
+  def log_out!
+    session[:user_id] = nil
+  end
 end
