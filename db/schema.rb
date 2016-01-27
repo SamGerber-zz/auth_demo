c031f115b850838030c222b9b0437b32b962e178
SamGerber <gerbyxvi@gmail.com>
2016-01-27 07:51:20 -0800

    Change password column to password digest

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127131424) do
+ActiveRecord::Schema.define(version: 20160127151027) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "username",   null: false
-    t.string   "password",   null: false
+    t.string   "username",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "password_digest", null: false
   end
 
 end
