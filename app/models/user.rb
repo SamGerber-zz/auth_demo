08384cb3df384d33fe018f7e75e49cdc19e0981e
SamGerber <gerbyxvi@gmail.com>
2016-01-27 08:16:17 -0800

    Improve password handling
     - Add password attribute (model only, NOT DB!)
     - #password=() setter also sets digest
     - validates password length, allowing nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,15 @@
 class User < ActiveRecord::Base
+  attr_reader :password
+
   validates :username, :password_digest, presence: true
-  #validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  def password=(password)
+    @password = password
+    self.password_digest = Digest::SHA2.hexdigest(password)
+  end
 
   def self.find_by_credentials(username:, password:)
-    #TODO
-
     user = find_by(username: username)
     return nil if user.blank?
     password_digest = Digest::SHA2.hexdigest(password)
